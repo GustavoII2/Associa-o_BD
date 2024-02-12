1068eacd330cc28b505752ed76b6bc80d305d0aa
GustavoII2 <gustavo.ayala@upi.edu.br>
2024-02-12 19:45:43 -0300

    inicio de projeto

---
+++ associacao/src/main/java/com/associacao/associacao/AssociacaoApplication.java
@@ -0,0 +1,13 @@
+package com.associacao.associacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociacaoApplication.class, args);
+	}
+
+}
